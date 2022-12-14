e391aca29296335a99f724048d35a861b73f9b2c
Andrés Salgado <andressalgado41@gmail.com>
2022-12-14 12:26:45 -0500

    issues app part 2-2

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const gitubApi = axios.create({
     baseURL:'https://api.github.com/repos/facebook/react',
     headers:{
-        Authorization: 'Bearer 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 '
+        // Authorization: 'Bearer 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 '
     }
 })
