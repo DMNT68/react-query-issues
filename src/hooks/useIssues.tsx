44e239dd5d4050f03e4adffbccaa7ddc64f4fbf2
Andrés Salgado <andressalgado41@gmail.com>
2022-12-16 16:42:02 -0500

    paginacion, infinite scroll

--- src/hooks/useIssues.tsx
+++ src/hooks/useIssues.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { Issue, State } from '../interfaces';
 import { gitubApi } from '../api/githubApi';
 import { useQuery } from '@tanstack/react-query';
@@ -6,9 +7,10 @@ import { sleep } from '../helpers/sleep';
 interface Props {
   state?: State;
   labels: string[];
+  page?: number;
 }
 
-const getIssues = async (labels: string[] = [], state?: State): Promise<Issue[]> => {
+const getIssues = async ({ labels, state, page = 1 }: Props): Promise<Issue[]> => {
   await sleep(2);
   const params = new URLSearchParams();
 
@@ -19,7 +21,7 @@ const getIssues = async (labels: string[] = [], state?: State): Promise<Issue[]>
     params.append('labels', labelString);
   }
 
-  params.append('page', '1');
+  params.append('page', page.toString());
   params.append('per_page', '5');
 
   const { data } = await gitubApi.get<Issue[]>('/issues', { params });
@@ -27,6 +29,31 @@ const getIssues = async (labels: string[] = [], state?: State): Promise<Issue[]>
 };
 
 export const useIssues = ({ state, labels }: Props) => {
-  const issuesQuery = useQuery(['issues', { state, labels }], () => getIssues(labels, state));
-  return { issuesQuery };
+  const [page, setPage] = useState(1);
+  useEffect(() => {
+    setPage(1);
+  }, [state, labels]);
+
+  const issuesQuery = useQuery(['issues', { state, labels, page }], () => getIssues({ labels, state, page }));
+
+  const nextPage = () => {
+    if (issuesQuery.data?.length === 0) return;
+    setPage(page + 1);
+  };
+
+  const prevPage = () => {
+    if (page > 1) setPage(page - 1);
+  };
+
+  return {
+    // Properties
+    issuesQuery,
+
+    // Getters
+    page: issuesQuery.isFetching ? 'loading' : page,
+
+    // Methods
+    nextPage,
+    prevPage,
+  };
 };
--- src/issues/views/ListViewInfinite.tsx
+++ src/issues/views/ListViewInfinite.tsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 import { IssueList } from '../components/IssueList';
 import { LabelPicker } from '../components/LabelPicker';
-import { useIssues } from '../../hooks';
+import { useIssuesInfinite } from '../../hooks';
 import { LoadingIcon } from '../../shared/components/LoadingIcon';
 import { State } from '../../interfaces';
 
-export const ListView = () => {
+export const ListViewInfinite = () => {
   const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
   const [state, setState] = useState<State>();
-  const { issuesQuery, page, nextPage, prevPage } = useIssues({ state, labels: selectedLabels });
+  const { issuesQuery } = useIssuesInfinite({ state, labels: selectedLabels });
 
   const onLabelChanged = (labelName: string) => {
     selectedLabels.includes(labelName) ? setSelectedLabels(selectedLabels.filter((label) => label != labelName)) : setSelectedLabels([...selectedLabels, labelName]);
@@ -17,16 +17,10 @@ export const ListView = () => {
   return (
     <div className="row mt-5">
       <div className="col-8">
-        {issuesQuery.isLoading ? <LoadingIcon /> : <IssueList issues={issuesQuery.data || []} state={state} onStateChange={(newState) => setState(newState)} />}
-        <div className="d-flex mt-2 justify-content-between align-items-center">
-          <button disabled={issuesQuery.isLoading} className="btn btn-outline-primary" onClick={prevPage}>
-            Prev
-          </button>
-          <span>{page}</span>
-          <button disabled={issuesQuery.isLoading} className="btn btn-outline-primary" onClick={nextPage}>
-            Next
-          </button>
-        </div>
+        {issuesQuery.isLoading ? <LoadingIcon /> : <IssueList issues={issuesQuery.data?.pages.flat() || []} state={state} onStateChange={(newState) => setState(newState)} />}
+        <button disabled={!issuesQuery.hasNextPage} className="btn btn-outline-primary mt-2" onClick={() => issuesQuery.fetchNextPage()}>
+          {!issuesQuery.isFetching ? 'Load More...' : 'Loading...'}
+        </button>
       </div>
 
       <div className="col-4">
